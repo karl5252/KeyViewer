41ca089e5b136e3746d75bbce8f8baff7ca13851
Karol <karl.michalik@gmail.com>
2020-10-11 21:39:15 +0200

    Added layout manager

--- src/javain24/SalutonFrame.java
+++ src/javain24/SalutonFrame.java
@@ -1,13 +1,28 @@
 package javain24;
 
 import javax.swing.*;
+import java.awt.*;
+import java.util.concurrent.Flow;
 
 public class SalutonFrame extends JFrame {
+
+    JButton okButton,cancelButton, abortButton;
+
     public SalutonFrame(){
         super("Saluton Mondo");
         setLookAndFeel();
         setSize(450,200);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
+        FlowLayout flowLayout = new FlowLayout();
+        setLayout(flowLayout);
+
+        okButton = new JButton("OK");
+        add(okButton);
+        cancelButton = new JButton("Cancel");
+        add(cancelButton);
+        abortButton = new JButton("Abort");
+        add(abortButton);
+
         setVisible(true);
     }
 
